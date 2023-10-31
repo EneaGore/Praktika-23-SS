57b3fdc69c880b039598f87a66da30d76000a3aa
EneaGore <73840596+EneaGore@users.noreply.github.com>
2023-10-31 03:44:58 +0100

    Update order_queue.php

--- server/order_queue.php
+++ server/order_queue.php
@@ -4,7 +4,6 @@ $order = $_POST["order"];
 
 $uniq = uniqid("user_");
 $db = new SQLite3("../database/queue.db");
-//$db->busyTimeout(5000);
 $db -> exec("PRAGMA journal_mode = wal;");
 $db->exec('PRAGMA synchronous = NORMAL;');
 $db -> exec("INSERT INTO orders(id,drink,status,uniqid) VALUES('{$id}','{$order}','in_queue','{$uniq}');");
