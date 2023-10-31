ec798f94e64cf21c402c818a3b56751d6f69eab3
EneaGore <73840596+EneaGore@users.noreply.github.com>
2023-10-31 03:35:27 +0100

    Delete async directory

--- async/server/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('content-type: application/json');
-print  json_encode(file_get_contents("../database/cocktails.json"));
-exit;
-?>
--- async/server/q_test.php
+++
@@ -1,6 +0,0 @@
-<?php
-file_put_contents("../database/db.txt","no way surely it still works right ");
-
-//$name = $_REQUEST["name"];
-//file_put_contents("db.txt",$name);
-?>
